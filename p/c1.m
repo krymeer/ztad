ec34bb96fa87fc52d914bbe2a5169becc723cebb
krymeer <krzysztof.radoslaw.osada@gmail.com>
2019-01-14 00:39:54 +0100

    Basics

---
+++ p/c1.m
@@ -0,0 +1,17 @@
+% Osada Krzysztof, 2019
+%
+% Parametry rozkladu poszczegolnych populacji: 
+% srednia, odchylenie standardowe, minimum, maksimum, 
+% mediana, pierwszy kwartyl i trzeci kwartyl
+
+clc; clear all; close all
+
+M = csvread('kursy_walut.csv', 2, 1)
+
+mean_               = mean(M)
+standard_deviation  = std(M)
+minimum             = min(M)
+maximum             = max(M)
+median_             = median(M)
+first_quartile      = quantile(M, 0.25)
+third_quartile      = quantile(M, 0.75)
